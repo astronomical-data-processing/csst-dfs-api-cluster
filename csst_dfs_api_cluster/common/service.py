4949f589589cfd2100b4a0b283187ba8df2585be
shoulinwei <weishoulin@astrolab.cn>
2022-12-29 20:01:37 +0800

    grpc message size to 10G

--- csst_dfs_api_cluster/common/service.py
+++ csst_dfs_api_cluster/common/service.py
@@ -6,8 +6,8 @@ class ServiceProxy:
         self.gateway = os.getenv("CSST_DFS_GATEWAY",'172.31.248.218:30880')
 
     def channel(self):
-        options = (('grpc.max_send_message_length', 1000 * 1024 * 1024),
-                    ('grpc.max_receive_message_length', 1000 * 1024 * 1024))     
+        options = (('grpc.max_send_message_length', 1024 * 1024 * 1024 * 10),
+                    ('grpc.max_receive_message_length', 1024 * 1024 * 1024 * 10))     
         # channel = grpc.insecure_channel(self.gateway, options = options, compression = grpc.Compression.Gzip)
         channel = grpc.insecure_channel(self.gateway, options = options)
         try:
